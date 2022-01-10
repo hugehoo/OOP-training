6b97c59f343b04ac30f78b5c793b489a344217ce
tbnsok40 <tbnsok40@gmail.com>
2022-01-10 23:28:48 +0900

    Utils 클래스 생성

--- src/com/company/ramen/Utils.java
+++ src/com/company/ramen/Utils.java
@@ -1,2 +1,32 @@
-package com.company.ramen;public class Utils {
+package com.company.ramen;
+
+public class Utils {
+
+    public String pot;
+    public String water;
+    public String chopsticks;
+
+    public String getPot() {
+        return pot;
+    }
+
+    public void setPot(String pot) {
+        this.pot = pot;
+    }
+
+    public String getWater() {
+        return water;
+    }
+
+    public void setWater(String water) {
+        this.water = water;
+    }
+
+    public String getChopsticks() {
+        return chopsticks;
+    }
+
+    public void setChopsticks(String chopsticks) {
+        this.chopsticks = chopsticks;
+    }
 }
