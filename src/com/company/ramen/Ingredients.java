c76ca71bbdaa423dc2bd5a76eef54faaf6de94e4
tbnsok40 <tbnsok40@gmail.com>
2022-01-11 00:20:37 +0900

    Ingredients 클래스 추가

--- src/com/company/ramen/Ingredients.java
+++ src/com/company/ramen/Ingredients.java
@@ -1,2 +1,10 @@
-package com.company.ramen;public class Ingredients {
+package com.company.ramen;
+
+public class Ingredients {
+
+    public Ingredients water;
+
+    public Ingredients getWater() {
+        return water;
+    }
 }
