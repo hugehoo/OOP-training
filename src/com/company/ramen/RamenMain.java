26863e4a3f661a72f4906a8b285748a099b6de34
tbnsok40 <tbnsok40@gmail.com>
2022-01-11 00:22:57 +0900

    RamenMain main() 메소드 작성

--- src/com/company/ramen/RamenMain.java
+++ src/com/company/ramen/RamenMain.java
@@ -2,7 +2,19 @@ package com.company.ramen;
 
 public class RamenMain {
 
-
     public static void main(String[] args) {
+        Ramen spicyRamen = new Ramen("신라면", "매운스프");
+        String ramen = spicyRamen.getRamen();
+        String soup = spicyRamen.getSoup();
+
+        Ingredients ingredients = new Ingredients();
+        Utils utils = new Utils();
+        Cook cook = new Cook();
+
+        cook.pourWaterInPot(utils.getPot(), ingredients.getWater());
+        cook.boilWater();
+        cook.putRamen(ramen);
+        cook.putSoup(soup);
+        cook.finish();
     }
 }
