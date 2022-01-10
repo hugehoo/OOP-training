a4954d9238d81ba993b0c4138a9a80fde2ad2b40
tbnsok40 <tbnsok40@gmail.com>
2022-01-11 00:21:07 +0900

    Utils 클래스 수정, chopstick 삭제

--- src/com/company/ramen/Utils.java
+++ src/com/company/ramen/Utils.java
@@ -2,31 +2,9 @@ package com.company.ramen;
 
 public class Utils {
 
-    public String pot;
-    public String water;
-    public String chopsticks;
+    public Utils pot;
 
-    public String getPot() {
+    public Utils getPot() {
         return pot;
-    }
-
-    public void setPot(String pot) {
-        this.pot = pot;
-    }
-
-    public String getWater() {
-        return water;
-    }
-
-    public void setWater(String water) {
-        this.water = water;
-    }
-
-    public String getChopsticks() {
-        return chopsticks;
-    }
-
-    public void setChopsticks(String chopsticks) {
-        this.chopsticks = chopsticks;
     }
 }
