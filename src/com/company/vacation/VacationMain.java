59825db30018b5f414bcf797ace51c7d9c2efd6f
tbnsok40 <tbnsok40@gmail.com>
2022-01-11 21:47:16 +0900

    VacationMain main() 메소드 작성

--- src/com/company/vacation/VacationMain.java
+++ src/com/company/vacation/VacationMain.java
@@ -1,2 +1,17 @@
-package com.company.vacation;public class VacationMain {
+package com.company.vacation;
+
+public class VacationMain {
+
+    public static void main(String[] args) {
+
+        public void selectVacation();
+        public void checkLeftDays ();
+        public boolean compareVacations ();
+        public void settingApprovalLine ();
+        public boolean getApproval ();
+        public void terminateApproval() ;
+        public void deductVacation ();
+        public void leftVacation ();
+
+    }
 }
