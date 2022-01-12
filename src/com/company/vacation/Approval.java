46a11bfd311c7ee5e2d3bad4f5a26ad6d853eed7
tbnsok40 <tbnsok40@gmail.com>
2022-01-13 00:17:43 +0900

    Approval class 작성

--- src/com/company/vacation/Approval.java
+++ src/com/company/vacation/Approval.java
@@ -1,2 +1,31 @@
-package com.company.vacation;public class Approval {
+package com.company.vacation;
+
+public class Approval {
+
+    Person person;
+
+    public Approval(Person person) {
+        this.person = person;
+    }
+
+    public boolean settingApprovalLine(Person person) {
+        String approvalLine = person.approvalLine;
+        return requestApproval(approvalLine);
+    }
+
+    public boolean requestApproval(String approvalLine) {
+
+        // logic 간략화 -> 결재라인에 따라 결재가 완료됐다고 하자.
+        if (true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void terminateApproval() {
+        return;
+    }
+
+
 }
