e93e0ead837ec2bb0612b649dc92b352b7c5ef44
tbnsok40 <tbnsok40@gmail.com>
2022-01-20 00:39:14 +0900

    Order Interface 생성

---
+++ src/com/company/baemin/Chicken.java
@@ -0,0 +1,2 @@
+package com.company.baemin;public class Chicken {
+}
---
+++ src/com/company/baemin/Order.java
@@ -0,0 +1,9 @@
+package com.company.baemin;
+
+public interface Order {
+
+    public void order(int orderCount);
+
+    public void info();
+
+}
