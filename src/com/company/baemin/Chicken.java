2c3f122457c53b7b82d931fd0f766c2737e7c6cd
tbnsok40 <tbnsok40@gmail.com>
2022-01-20 00:39:31 +0900

    Chickent 클래스 생성

--- src/com/company/baemin/Chicken.java
+++ src/com/company/baemin/Chicken.java
@@ -1,2 +1,35 @@
-package com.company.baemin;public class Chicken {
+package com.company.baemin;
+
+public class Chicken implements Order{
+
+    String storeName;
+    int totalAmount = 0;
+    int orderCount;
+
+    public Chicken(String storeName) {
+        this.storeName = storeName;
+    }
+
+    @Override
+    public void order(int orderCount) {
+        ++ this.orderCount;
+        this.totalAmount += 20000;
+    }
+
+    @Override
+    public void info() {
+
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public int getTotalAmount() {
+        return totalAmount;
+    }
+
+    public int getOrderCount() {
+        return orderCount;
+    }
 }
