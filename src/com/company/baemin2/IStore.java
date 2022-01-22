476709c02b6037b6dcb1ff3699848e5ab2454271
tbnsok40 <tbnsok40@gmail.com>
2022-01-22 12:06:44 +0900

    IStore 인터페이스 추가

--- src/com/company/baemin2/IStore.java
+++ src/com/company/baemin2/IStore.java
@@ -1,2 +1,9 @@
-package com.company.baemin2;public interface IStore {
+package com.company.baemin2;
+
+public interface IStore {
+
+    public int ordering(String menu, int orderCount, int money);
+
+    public void info();
+
 }
