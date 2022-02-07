a7bd5a8e599da00eccace1302661855e87c9471d
tbnsok40 <tbnsok40@gmail.com>
2022-02-08 06:44:30 +0900

    Static 메서드 제거

--- src/com/company/movie/MovieMain.java
+++ src/com/company/movie/MovieMain.java
@@ -2,13 +2,10 @@ package com.company.movie;
 
 public class MovieMain {
 
-    public static Theater chooseTheater(Integer integer) {
-        return new Theater(integer);
-    }
-
     public static void main(String[] args) {
-        Theater theater = chooseTheater(3);
-        String movie = theater.chooseMovie(theater, "어바웃타임");
+        Theater theater = new Theater(3);
+        String[] movieOnAir = theater.getMovieList(theater.getTheaterCode());
+        String movie = theater.chooseMovie(movieOnAir, "어바웃타임");
         Pay pay = new Pay();
         int amount = pay.order(2, "Adult", "A");
     }
