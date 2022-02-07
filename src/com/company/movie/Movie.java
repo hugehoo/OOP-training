24ad66f2a4ab0cf048bf0454648397d7363764a7
tbnsok40 <tbnsok40@gmail.com>
2022-02-08 00:22:32 +0900

    Moviemain 작성

--- src/com/company/movie/Movie.java
+++
@@ -1,2 +0,0 @@
-package com.company.movie;public class Movie {
-}
--- src/com/company/movie/MovieMain.java
+++ src/com/company/movie/MovieMain.java
@@ -1,2 +1,16 @@
-package com.company.movie;public class MovieMain {
+package com.company.movie;
+
+public class MovieMain {
+
+    public static Theater chooseTheater(Integer integer) {
+        return new Theater(integer);
+    }
+
+    public static void main(String[] args) {
+        Theater theater = chooseTheater(3);
+        String movie = theater.chooseMovie(theater, "어바웃타임");
+        Pay pay = new Pay();
+        int amount = pay.order(2, "Adult", "A");
+    }
+
 }
