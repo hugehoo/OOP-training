5ac171df3aed3170c4df3f0ac11175a08eca4951
tbnsok40 <tbnsok40@gmail.com>
2022-02-12 14:17:56 +0900

    [AirPurifyMain] powerOn, PowerOff 메소드를 powerOnOff 메소드 하나로 통일

--- src/com/company/AirPurifier/AirPurifyMain.java
+++ src/com/company/AirPurifier/AirPurifyMain.java
@@ -5,9 +5,9 @@ public class AirPurifyMain {
 
     public static void main(String[] args) {
         AirPurifier airPurifier = new AirPurifier();
-        airPurifier.powerOn(); // airPurifier 의 상태(power) 변경
+        airPurifier.powerOnOff(); // airPurifier 의 상태(power) 변경
         airPurifier.purifyLevelUp();
         airPurifier.filterLevelUp();
-        airPurifier.powerOff();
+        airPurifier.powerOnOff();
     }
 }
