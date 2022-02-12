f23e2a87d2911e652d6b8ce2452baa0d8bbc1d45
tbnsok40 <tbnsok40@gmail.com>
2022-02-12 14:10:33 +0900

    [AirPurifyMain] AirPurifyMain 프로세스 코드 작성

--- src/com/company/AirPurifier/AirPurifyMain.java
+++ src/com/company/AirPurifier/AirPurifyMain.java
@@ -1,2 +1,13 @@
-package com.company.AirPurifier;public class AirPurifyMain {
+package com.company.AirPurifier;
+
+public class AirPurifyMain {
+
+
+    public static void main(String[] args) {
+        AirPurifier airPurifier = new AirPurifier();
+        airPurifier.powerOn(); // airPurifier 의 상태(power) 변경
+        airPurifier.purifyLevelUp();
+        airPurifier.filterLevelUp();
+        airPurifier.powerOff();
+    }
 }
