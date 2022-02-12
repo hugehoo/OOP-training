364198b9e396da44986e41667333daaaf3779c78
tbnsok40 <tbnsok40@gmail.com>
2022-02-12 14:21:03 +0900

    [AirPurifier] purifyLevelUp() 메서드 구현

--- src/com/company/AirPurifier/AirPurifier.java
+++ src/com/company/AirPurifier/AirPurifier.java
@@ -3,8 +3,19 @@ package com.company.AirPurifier;
 public class AirPurifier {
 
     private Boolean powerStatus = false;
+    private int purifyLevel = 1;
 
     public void powerOnOff() {
         powerStatus = !powerStatus;
+    }
+
+    public void purifyLevelUp() {
+        while (powerStatus) {
+            if (purifyLevel != 3) {
+                ++purifyLevel;
+            } else {
+                purifyLevel = 1;
+            }
+        }
     }
 }
