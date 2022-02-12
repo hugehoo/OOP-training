f77aea65dfdbeac33e1702d7d1e14228462c8a69
tbnsok40 <tbnsok40@gmail.com>
2022-02-12 14:42:25 +0900

    [AirPurifyMain] AirPurify 동작 구현

--- src/com/company/AirPurifier/AirPurifyMain.java
+++ src/com/company/AirPurifier/AirPurifyMain.java
@@ -4,10 +4,21 @@ public class AirPurifyMain {
 
 
     public static void main(String[] args) {
-        AirPurifier airPurifier = new AirPurifier();
-        airPurifier.powerOnOff(); // airPurifier 의 상태(power) 변경
+        AirPurifier airPurifier = new AirPurifier(false, 1, 1);
+
+        airPurifier.powerOnOff();
         airPurifier.purifyLevelUp();
         airPurifier.filterLevelUp();
+        airPurifier.showStatus();
+
+        airPurifier.purifyLevelUp();
+        airPurifier.filterLevelUp();
+        airPurifier.showStatus();
+
+        airPurifier.filterLevelUp();
+        airPurifier.showStatus();
+
         airPurifier.powerOnOff();
+        airPurifier.showStatus();
     }
 }
